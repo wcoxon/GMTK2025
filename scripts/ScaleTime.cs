d742971f42644f77d1841fce62b44cf5e710d8bf
Grant Westfield <pollodeastra@gmail.com>
2025-07-31 13:02:05 +0100

    Scale Time
    You can now scale time using the buttons. Also updates the .NET SDK target, and Godot Version

---
+++ scripts/ScaleTime.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class ScaleTime : Button
+{
+    /// Take a number and set the world timescale in player view to it, that class handles all the important shit.
+
+    [Export] int newTimeScale;
+
+
+
+    public override void _Pressed()
+    {
+        base._Pressed();
+        PlayerView.instance.setWorldSpeed(newTimeScale);
+    }
+
+
+
+}
