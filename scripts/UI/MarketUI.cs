2d3b15ee86135c3c158db40f80527abc3f975e6d
Liassica <115422798+Liassica@users.noreply.github.com>
2025-08-02 22:25:11 -0500

    show item descriptions on hover

--- scripts/UI/MarketUI.cs
+++ scripts/UI/MarketUI.cs
@@ -10,13 +10,14 @@ public partial class MarketUI : Panel
     [Export] Label productionLabel;
     [Export] Label consumptionLabel;
     [Export] RichTextLabel priceLabel;
-
+    [Export] Label descriptionLabel;
 
     [Export] public string ItemName { get => nameLabel.Text; set => nameLabel.Text = value; }
     public int Stock { set => stockLabel.Text = value.ToString(); }
     public int Production { set => productionLabel.Text = value.ToString(); }
     public int Consumption { set => consumptionLabel.Text = value.ToString(); }
     public int Price { set => priceLabel.Text = value.ToString(); }
+    [Export] public string ItemDescription { get => descriptionLabel.Text; set => descriptionLabel.Text = value; }
     
     public int PriceOffset {
         set
@@ -24,5 +25,16 @@ public partial class MarketUI : Panel
             if (value != 0) priceLabel.Text += value < 0 ? $" [color=red]({value})[/color]" : $" [color=green](+{value})[/color]"; // this adds a red (-x) or green (+x) to the price label if price is lower or higher than item base value (rn that's 5 for everything but dw)
         }
     }
-	
+
+    public void onHBoxSortChildren() {
+        descriptionLabel.OffsetLeft = GetParent<VBoxContainer>().Size.X + 20;
+    }
+
+    public void onMouseEntered(){
+        descriptionLabel.Show();
+    }
+
+    public void onMouseExited(){
+        descriptionLabel.Hide();
+    }
 }
